3574d0343706314fcc75ec4eae8f6d93017a9a4e
Longqi Cai <longqicai@gmail.com>
2016-06-14 16:54:57 -0400

    add stat apis

--- ocd/network/_scapy.py
+++ ocd/network/_scapy.py
@@ -9,11 +9,11 @@ TODO: purpose
 
 class ScapyWrapper(object):
     def sniff(self, timeout=5):
-        # TODO
+        # TODO@xiaolong
         pass
 
     def load(self, capfile):
-        # TODO
+        # TODO@xiaolong
         pass
 
     def unique_macs(self, time_slot=None, ip=None):
@@ -27,7 +27,7 @@ class ScapyWrapper(object):
         Returns:
             set(str): mac addresses
         """
-        # TODO
+        # TODO@xiaolong
         pass
 
     def unique_ips(self, time_slot=None, src_ip=None, dst_ip=None):
@@ -47,7 +47,7 @@ class ScapyWrapper(object):
         Returns:
             set(str): unique ip addresses
         """
-        # TODO
+        # TODO@xiaolong
         pass
 
     def unique_protocols(self, time_slot=None, src_ip=None, dst_ip=None):
@@ -62,7 +62,7 @@ class ScapyWrapper(object):
         Returns:
             set(str): unique protocols
         """
-        # TODO
+        # TODO@xiaolong
         pass
 
     def unique_ports(self, time_slot=None, ip=None):
@@ -76,5 +76,68 @@ class ScapyWrapper(object):
         Returns:
             set(int): unique ports
         """
-        # TODO
+        # TODO@xiaolong
+        pass
+
+    def stat_macs(self, time_slot=None, ip=None):
+        """Get a dict of MAC addresses with the freq it appears
+
+        Args:
+            time_slot (optional[tuple]): a tuple of two specifying the
+                start and end time in the format of timestamp
+            ip (optional[list(str)]):
+
+        Returns:
+            dict{str->int}: mac addresses and the freq it appears
+        """
+        # TODO@manga
+        pass
+
+    def stat_ips(self, time_slot=None, src_ip=None, dst_ip=None):
+        """Get a dict of ips and their freq
+
+        It can be either src ip or dst ip. If src ip is specified,
+        then dst ips are returned, vice versa. If none is specified,
+        all unique ips will be returned.
+
+        Args:
+            time_slot (optional[tuple]): a tuple of two specifying the
+                start and end time in the format of timestamp
+            src_ip (optional[list(str), or single str]): src ip(s)
+            dst_ip (optional[list(str), or single str]): dst ip(s),
+                src_ip and dst_ip shouldn't be both specified
+
+        Returns:
+            dict{str->int}: ip addresses and freq
+        """
+        # TODO@manga
+        pass
+
+    def stat_protocols(self, time_slot=None, src_ip=None, dst_ip=None):
+        """Get a dict of protocols and freq
+
+        Args:
+            time_slot (optional[tuple]): a tuple of two specifying the
+                start and end time in the format of timestamp
+            src_ip (optional[list(str), or single str]): src ip(s)
+            dst_ip (optional[list(str), or single str]): dst ip(s)
+
+        Returns:
+            dict{str->int}: unique protocols
+        """
+        # TODO@manga
+        pass
+
+    def stat_ports(self, time_slot=None, ip=None):
+        """Get a dict of ports within the capture
+
+        Args:
+            time_slot (optional[tuple]): a tuple of two specifying the
+                start and end time in the format of timestamp
+            ip (optional[list(str), or single str]): the corresponding ip(s)
+
+        Returns:
+            dict{int->int}: unique ports
+        """
+        # TODO@manga
         pass
--- ocd/sensor.py
+++ ocd/sensor.py
@@ -123,6 +123,87 @@ class NetworkSensor(Sensor):
             ip = [ip]
         return self.wrapper.unique_ports(time_slot, ip)
 
+    def stat_macs(self, time_slot=None, ip=None):
+        """Get a dict of MAC addresses with the freq it appears
+
+        Args:
+            time_slot (optional[tuple]): a tuple of two specifying the
+                start and end time in the format of timestamp
+            ip (optional[list(str)]):
+
+        Returns:
+            dict{str->int}: mac addresses and the freq it appears
+        """
+        if ip is not None and not isinstance(ip, list):
+            ip = [ip]
+        return self.wrapper.stat_macs(time_slot, ip)
+
+    def stat_ips(self, time_slot=None, src_ip=None, dst_ip=None):
+        """Get a dict of ips and their freq
+
+        It can be either src ip or dst ip. If src ip is specified,
+        then dst ips are returned, vice versa. If none is specified,
+        all unique ips will be returned.
+
+        Args:
+            time_slot (optional[tuple]): a tuple of two specifying the
+                start and end time in the format of timestamp
+            src_ip (optional[list(str), or single str]): src ip(s)
+            dst_ip (optional[list(str), or single str]): dst ip(s),
+                src_ip and dst_ip shouldn't be both specified
+
+        Returns:
+            dict{str->int}: ip addresses and freq
+        """
+        if src_ip is not None and dst_ip is not None:
+            raise Exception("src_ip and dst_ip shouldn't be both specified!")
+        if src_ip is not None and not isinstance(src_ip, list):
+            src_ip = [src_ip]
+        if dst_ip is not None and not isinstance(dst_ip, list):
+            dst_ip = [dst_ip]
+        return self.wrapper.stat_ips(time_slot, src_ip, dst_ip)
+
+    def stat_protocols(self, time_slot=None, src_ip=None, dst_ip=None):
+        """Get a dict of protocols and freq
+
+        Args:
+            time_slot (optional[tuple]): a tuple of two specifying the
+                start and end time in the format of timestamp
+            src_ip (optional[list(str), or single str]): src ip(s)
+            dst_ip (optional[list(str), or single str]): dst ip(s)
+
+        Returns:
+            dict{str->int}: unique protocols
+        """
+        if src_ip is not None and dst_ip is not None:
+            raise Exception("src_ip and dst_ip shouldn't be both specified!")
+        if src_ip is not None and not isinstance(src_ip, list):
+            src_ip = [src_ip]
+        if dst_ip is not None and not isinstance(dst_ip, list):
+            dst_ip = [dst_ip]
+        return self.wrapper.stat_protocols(time_slot, src_ip, dst_ip)
+
+    def stat_ports(self, time_slot=None, ip=None):
+        """Get a dict of ports within the capture
+
+        Args:
+            time_slot (optional[tuple]): a tuple of two specifying the
+                start and end time in the format of timestamp
+            ip (optional[list(str), or single str]): the corresponding ip(s)
+
+        Returns:
+            dict{int->int}: unique ports
+        """
+        if ip is not None and not isinstance(ip, list):
+            ip = [ip]
+        return self.wrapper.stat_ports(time_slot, ip)
+
+
+class HostSensor(Sensor):
+    def sniff(self):
+        # TODO
+        pass
+
 
 class MotionSensor(Sensor):
     pass
