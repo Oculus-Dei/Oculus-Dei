99484fe2169569e1857666b6e1eb7d5925b4d965
Longqi Cai <longqicai@gmail.com>
2016-07-12 14:59:13 -0400

    add travis

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Occulus-Dei Sensor API
+[![Build Status](https://travis-ci.org/Oculus-Dei/Oculus-Dei.svg?branch=master)](https://travis-ci.org/Oculus-Dei/Oculus-Dei)
 
---
+++ test/test_device.py
@@ -0,0 +1,13 @@
+# encoding: utf-8
+"""
+Created by misaka-10032 (longqic@andrew.cmu.edu).
+
+TODO: purpose
+"""
+
+import ocd.device
+
+
+def test_device():
+    d = ocd.device.Device()
+    assert True
