f7b0c06f3cc331cf16a288172a76f3562dfff144
Longqi Cai <longqicai@gmail.com>
2016-07-12 15:41:39 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from distutils.core import setup
+
+setup(name='ocd',
+      version='1.0',
+      description='Oculus-Dei sensor API',
+      author='Oculus-Dei',
+      author_email='longqic@andrew.cmu.edu',
+      url='https://github.com/Oculus-Dei/Oculus-Dei',
+      packages=['ocd'],
+     )
+
